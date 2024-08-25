f1bd763105d8660391a9398397280f918fceca9f
Hamza Sajid <hamzasajid1165@gmail.com>
2024-08-25 11:48:28 +0500

    COMMIT kar dy bhaiiiiii

--- src/custom_components/Alert.js
+++ src/custom_components/Alert.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 function Alert(props) {
@@ -20,26 +19,3 @@ function Alert(props) {
 }
 
 export default Alert
-=======
-import React from 'react'
-
-function Alert(props) {
-
-    const capt = (type) => {
-        let word = (type).toLowerCase()  ;
-        return word.charAt(0).toUpperCase() + word.slice(1)  
-    }
-
-
-    return (
-        <div style={{ height: '50px'}}>
-            {props.alert && 
-            <div className="alert alert-warning alert-dismissible fade show" style={{backgroundColor : '#dad7fa'}} role="alert">
-                <strong>{capt(props.alert.type)}</strong>: {props.alert.message}
-            </div>}
-        </div>
-  )
-}
-
-export default Alert
->>>>>>> a7105a81bc92301a631460c80bc5295f879f88f4
