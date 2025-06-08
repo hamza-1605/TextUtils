d95697a6d5c9e481f8312edd46c866c2aa0e0eff
Hamza Sajid <hamzasajid1165@gmail.com>
2025-06-09 04:05:17 +0500

    Fixed Dark Navbar color

--- src/custom_components/Navbar.js
+++ src/custom_components/Navbar.js
@@ -4,7 +4,21 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
 
+    const texti = document.getElementsByClassName('texti');
+    
+    const whiteTextColor = () => {
+        for (let i = 0 ; i < texti.length; i++) {
+            texti[i].style.color = 'white' ;
+        }
+    }
+    const blackTextColor = () => {
+        for (let i = 0 ; i < texti.length; i++) {
+            texti[i].style.color = 'black' ;
+        }
+    }
+    
 
+    // objects for css of buttons in the navbar
     const light = {
         color: 'black',
         backgroundColor: 'white',
@@ -51,7 +65,7 @@ export default function Navbar(props) {
         <nav className={`navbar navbar-expand-lg navbar-light`} style={ navBgc(props.mode) }>
         <div className="container-fluid">
             {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
-            <a className="navbar-brand" href="#">{props.title}</a>
+            <a className="navbar-brand texti" href="#">{props.title}</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
@@ -59,7 +73,7 @@ export default function Navbar(props) {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                 {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active texti" aria-current="page" href="#">Home</a>
                 </li>
                 <li className="nav-item">
                 {/* <Link className="nav-link" to="/about">About</Link> */}
@@ -70,13 +84,13 @@ export default function Navbar(props) {
                 <button className="btn btn-outline-success" type="submit">Search</button>
                 </form> */}
             <div className="">
-                <button type="button" onClick={ () => {props.toggle('white')}} className="mx-2 btn btn" style={light}>Light</button>
+                <button type="button" onClick={ () => {props.toggle('white') ; blackTextColor()}} className="mx-2 btn btn" style={light}>Light</button>
 
-                <button type="button" onClick={ () => {props.toggle('black')}} className="mx-2  btn btn" style={dark}>Dark</button>
+                <button type="button" onClick={ () => {props.toggle('black') ; whiteTextColor()}} className="mx-2  btn btn" style={dark}>Dark</button>
                 
-                <button type="button" onClick={ () => {props.toggle('rgb(38 26 151)')}} className="mx-2  btn btn" style={blue}>Blue</button>
+                <button type="button" onClick={ () => {props.toggle('rgb(38 26 151)'); whiteTextColor()} } className="mx-2  btn btn" style={blue}>Blue</button>
                 
-                <button type="button" onClick={ () => {props.toggle('#D75B71')}} className="mx-2  btn btn" style={pink}>Pink</button>
+                <button type="button" onClick={ () => {props.toggle('#D75B71'); whiteTextColor()}} className="mx-2  btn btn" style={pink}>Pink</button>
             </div>
             {/* <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
                 <input className="form-check-input" onClick={ props.toggle } type="checkbox" role="switch" id="flexSwitchCheckDefault" />
